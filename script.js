0ab7d24c612d354925226fc236344ccafbe830f7
Mansi Ramsingh Tilatiya <tilatemeena@gmail.com>
2026-09-13 15:52:38 +0530

    Add welcome message and button click functions

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome message
+console.log("Welcome to my website!");
+
+// Button click function
+function showMessage() {
+    alert("Welcome! Thanks for visiting my website 😊");
+}
+
+// Change heading text
+function changeText() {
+    const heading = document.getElementById("heading");
+    heading.textContent = "Welcome to My Website!";
+}
