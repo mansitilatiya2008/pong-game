a1a8662ca62e519794cc3d8d6c81763bc6ae0e9b
Mansi Ramsingh Tilatiya <tilatemeena@gmail.com>
2026-09-14 12:50:31 +0530

    Implement game start and restart functionality
    Added game control functions to manage game state.

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ function changeText() {
     const heading = document.getElementById("heading");
     heading.textContent = "Welcome to My Website!";
 }
+let gameStarted = false;
+
+function startGame() {
+    if (!gameStarted) {
+        gameStarted = true;
+        gameLoop();
+    }
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    playerY = canvas.height / 2 - paddleHeight / 2;
+    computerY = canvas.height / 2 - paddleHeight / 2;
+
+    resetBall();
+
+    gameStarted = true;
+}
